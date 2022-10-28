5cbb7ca0f539c593ba51da3312ac5b1c17ab4d26
Christian <cmanalo.io@outlook.com>
2022-10-28 05:55:34 -0700

    update the styling to the data

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ let searchBtn = document.getElementById("search-button");
 let countryInput = document.getElementById("country-input");
 
 searchBtn.addEventListener("click", () =>{
-    let countryName = "India";
+    let countryName = countryInput.value;
     let url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true `;
-    console.log(url)
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
@@ -15,6 +14,59 @@ searchBtn.addEventListener("click", () =>{
             console.log(data[0].continents[0]);
             console.log(Object.keys(data[0].currencies)[0]);
             console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-            console.log(Object.values(data[0].languages).toString().split(',').join(', '));
+            console.log(Object.values(data[0].languages).join(', '));
+            result.innerHTML = `
+            
+            <img src= "${data[0].flags.svg}" class= "flag-img"> 
+            <h2> ${data[0].name.common}</h2>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4> Capital: </h4>
+                    <span> ${data[0].capital[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4> Continent: </h4>
+                    <span> ${data[0].continents[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4> Population: </h4>
+                    <span> ${data[0].population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+                </div>
+            </div>
+            <div class="wrapper">   
+                <div class="data-wrapper">
+                    <h4> Currency: </h4>
+                    <span> ${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}
+                    </span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4> Common Languages: </h4>
+                    <span> ${Object.values(data[0].languages).toString().split(',').join(', ')}</span>
+                </div>
+            </div>
+            `;
+        })
+
+        .catch(()=>{
+            if (countryName.length ==0){
+                result.innerHTML = `<h3> The input field cannot be empty </h3>`;}
+            else{
+                result.innerHTML = ` <h3> Please enter a valid input </h3>`
+            }
+            
         });
+
+
+
+         
+                
+                
+
+        
 });
